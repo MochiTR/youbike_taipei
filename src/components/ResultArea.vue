ed3cabb24160f7a1445536d5f827ca9fdbbd2252
Mochi <mochitreets0304@gmail.com>
2022-08-08 17:34:00 +0800

    根據選擇區域顯示站點

--- src/components/ResultArea.vue
+++ src/components/ResultArea.vue
@@ -2,37 +2,13 @@
 <!-- resultArea -->
    <div class="result-area">
    <ul class="result-list">
-     <li>
-      <p>YouBike2.0_捷運科技大樓站</p>
-        <div class="result-info">站點停車格數量:28</div>
-        <div class="result-info">站點目前車輛數:9</div>
-        <div class="result-info">更新時間:2022-08-08 12:41:13</div>
-        <div class="result-info">地點:復興南路二段235號前</div>
-        <div class="result-info">空位數量:19</div>
-      </li>
-     <li>
-      <p>YouBike2.0_捷運科技大樓站</p>
-        <div class="result-info">站點停車格數量:28</div>
-        <div class="result-info">站點目前車輛數:9</div>
-        <div class="result-info">更新時間:2022-08-08 12:41:13</div>
-        <div class="result-info">地點:復興南路二段235號前</div>
-        <div class="result-info">空位數量:19</div>
-      </li>
-      <li>
-      <p>YouBike2.0_捷運科技大樓站</p>
-        <div class="result-info">站點停車格數量:28</div>
-        <div class="result-info">站點目前車輛數:9</div>
-        <div class="result-info">更新時間:2022-08-08 12:41:13</div>
-        <div class="result-info">地點:復興南路二段235號前</div>
-        <div class="result-info">空位數量:19</div>
-      </li>
-      <li>
-      <p>YouBike2.0_捷運科技大樓站</p>
-        <div class="result-info">站點停車格數量:28</div>
-        <div class="result-info">站點目前車輛數:9</div>
-        <div class="result-info">更新時間:2022-08-08 12:41:13</div>
-        <div class="result-info">地點:復興南路二段235號前</div>
-        <div class="result-info">空位數量:19</div>
+     <li v-for="result in filterResult" :key="result.sno">
+      <p>{{result.sna}}</p>
+        <div class="result-info">站點停車格數量:{{result.tot}}</div>
+        <div class="result-info">站點目前車輛數:{{result.sbi}}</div>
+        <div class="result-info">更新時間:{{result.mday}}</div>
+        <div class="result-info">地點:{{result.ar}}</div>
+        <div class="result-info">空位數量:{{result.bemp}}</div>
       </li>
    </ul>
   </div>
@@ -42,6 +18,11 @@
 export default {
   data () {
     return {}
+  },
+  computed: {
+    filterResult () {
+      return this.$store.getters.filterResult
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,15 @@ import axios from 'axios'
 
 export default createStore({
   state: {
+    // 選擇的區域/搜尋的名稱
     currentArea: '',
+    // 根據currentArea回傳的結果
     resultList: [],
+    // 所有YouBike資料
     allData: []
   },
   getters: {
+    // 將allData過濾(無重複名稱)
     filterData (state) {
       const result = {}
 
@@ -16,6 +20,12 @@ export default createStore({
       })
 
       return Object.keys(result)
+    },
+    // 將allData過濾(currentArea)
+    filterResult (state) {
+      return state.allData.filter(function (item) {
+        return item.sarea === state.currentArea
+      })
     }
   },
   mutations: {
