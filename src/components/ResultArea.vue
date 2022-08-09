2c7dcaac8a1ec74c4f3ec659b4dcc383c1028e7e
Mochi <mochitreets0304@gmail.com>
2022-08-09 15:53:08 +0800

    搜尋結果頁碼

--- src/components/ResultArea.vue
+++ src/components/ResultArea.vue
@@ -2,7 +2,7 @@
 <!-- resultArea -->
    <div class="result-area">
    <ul class="result-list">
-     <li v-for="result in filterResult" :key="result.sno">
+     <li v-for="result in currData" :key="result.sno">
       <p>{{result.sna}}</p>
         <div class="result-info">站點停車格數量:{{result.tot}}</div>
         <div class="result-info">站點目前車輛數:{{result.sbi}}</div>
@@ -11,17 +11,55 @@
         <div class="result-info">空位數量:{{result.bemp}}</div>
       </li>
    </ul>
+   <div class="pag">
+   <button v-for="btn in pags" :key="btn" v-on:click="setListData(filterResult, eachPage, btn)">{{btn}}</button>
+   </div>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      currPage: 1,
+      arrFilter: '',
+      eachPage: 10,
+      currData: '',
+      pags: []
+    }
   },
   computed: {
     filterResult () {
       return this.$store.getters.filterResult
+    }
+  },
+  methods: {
+    // 處理頁數對應資料
+    setListData (all, much, page) {
+      // 處理放置資料,第一頁1~10,第二頁10~20..
+      if (page > 1) {
+        this.arrFilter = all.slice(page - 1 + '0', much * page)
+      } else {
+        this.arrFilter = all.slice(page - 1, much * page)
+      }
+      this.currData = this.arrFilter.map(function (item) {
+        return item
+      })
+    },
+    setPag (all, much) {
+      this.pags = []
+      // 計算頁碼數量
+      const pagNum = Math.ceil(all.length / much)
+      // 每一頁生成一個pagbtn
+      for (let i = 1; i <= pagNum; i++) {
+        this.pags.push(i)
+      }
+    }
+  },
+  watch: {
+    filterResult () {
+      this.setListData(this.filterResult, this.eachPage, this.currPage)
+      this.setPag(this.filterResult, this.eachPage)
     }
   }
 }
