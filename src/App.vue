5776d79805f61c6a6a241fea0b02e7029af35288
Mochi <mochitreets0304@gmail.com>
2022-08-10 14:04:45 +0800

    燈箱元件

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
    <h1 class="title">Taipei YouBike 查詢系統</h1>
-  </div>
+  <LightBox ref="lightbox"></LightBox>
   <SearchArea></SearchArea>
-  <ResultArea></ResultArea>
+  <ResultArea @emit-map="openLightbox"></ResultArea>
+  </div>
 </template>
 
 <script>
 import SearchArea from '@/components/SearchArea.vue'
 import ResultArea from '@/components/ResultArea.vue'
+import LightBox from './components/LightBox.vue'
 
 export default {
   name: 'App',
   components: {
     SearchArea,
-    ResultArea
+    ResultArea,
+    LightBox
+  },
+  methods: {
+    openLightbox () {
+      this.$refs.lightbox.showLightbox()
+    }
   }
 }
 </script>
---
+++ src/components/LightBox.vue
@@ -0,0 +1,68 @@
+<template>
+<transition name='lbox'>
+    <div class='wrap' v-if='isOpen'>
+    <img :src="img">
+    <button class="closeBtn" @click="closeLightbox">{{close}}</button>
+    </div>
+</transition>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isOpen: false,
+      img: 'https://images.unsplash.com/photo-1659983732775-51adb59b0263?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      close: 'X'
+    }
+  },
+  methods: {
+    showLightbox () {
+      this.isOpen = true
+    },
+    closeLightbox () {
+      this.isOpen = false
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+.wrap {
+  position:fixed;
+  background-color: rgba(0, 0, 0, 0.7);
+  height: 100%;
+  width: 100%;
+  top:0;
+  left:0;
+  z-index:333;
+  display:flex;
+}
+img {
+  margin:auto;
+  justify-content: center;
+  align-items: center;
+  position:relative;
+}
+.closeBtn {
+  position:absolute;
+  top:0;
+  right:0;
+  padding:1em;
+  color:white;
+  font-size: 24px;
+  cursor: pointer;
+  background: transparent;
+  border-style: none;
+}
+.lbox-enter-from,.lbox-leave-to {
+  opacity:0;
+}
+.lbox-enter-active,.lbox-leave-active {
+  transition:opacity 0.6s;
+}
+.lbox-enter-to,.lbox-leave-from {
+  opacity:1;
+}
+</style>
